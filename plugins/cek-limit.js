d9323a5576e3d6c325a748dcbf78f430a6f2606a
kimkiro <149228321+Whohn@users.noreply.github.com>
2025-02-19 03:27:49 +0700

    Add files via upload

--- plugins/cek-limit.js
+++ plugins/cek-limit.js
@@ -1,1 +1,10 @@
-vv
+const axios = require('axios')
+
+let handler = async (m, { belz, text, reply }) => {
+    reply(`Hello Worlds`) 
+}
+
+handler.command = ["limit", "cek-limit"];
+handler.owner = true
+
+module.exports = handler;
